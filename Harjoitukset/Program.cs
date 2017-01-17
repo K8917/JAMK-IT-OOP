c39e9c8366e2e002b68df565154332939d8e56f8
K8917 <K8917@student.jamk.fi>
2017-01-17 14:36:09 +0200

    Add files via upload

---
+++ Harjoitukset/Program.cs
@@ -0,0 +1,34 @@
+﻿// Ohjelma: Nimet aakkosjarjestykseen
+
+using System;
+
+namespace Harjoitukset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Anna kayttajien etunimet: ");
+
+            string[] nimet = new string[5];
+            for (int i = 0; i < 5; ++i)
+            {
+                nimet[i] = Console.ReadLine();
+            }
+
+            Console.Write("Annoit nimet: ");
+            for (int i = 0; i < 5; ++i)
+            {
+                Console.Write(nimet[i] + " ");
+            }
+
+            Array.Sort(nimet);
+            Console.Write("\nNimet aakkosjarjestyksessa: ");
+            for (int i = 0; i < 5; ++i)
+            {
+                Console.Write(nimet[i] + " ");
+            }
+            Console.Write('\n');
+        }
+    }
+}
