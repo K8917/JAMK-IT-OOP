cb1e11efb13f189ef41bca2effd507452fdccd68
K8917 <K8917@K8917>
2017-01-23 17:55:28 +0200

    Harj1 done.

---
+++ OOP-Harj/Kiuas.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Harj
+{
+    class Kiuas
+    {
+        private bool onOff_;
+        private uint temp_;
+        private uint steam_;
+
+        public Kiuas ()
+        {
+            this.onOff_ = false;
+            this.temp_ = 0;
+            this.steam_ = 0;
+        }
+
+        public Kiuas(uint temp, uint steam)
+        {
+            this.temp_ = temp;
+            this.steam_ = steam;
+        }
+
+        public bool TurnOnOff()
+        {
+            if (this.onOff_ == false)
+            {
+                this.onOff_ = true;
+                Console.WriteLine("Kiuas on paalla");
+            }
+            else
+            {
+                this.onOff_ = false;
+                Console.WriteLine("Kiuas on pois paalta");
+            }
+
+            return this.onOff_;
+        }
+
+        public void SetTemp(uint temp)
+        {
+            this.temp_ = temp;
+        }
+
+        public void SetSteam(uint steam)
+        {
+            this.steam_ = steam;
+        }
+
+        public void PrintInfo()
+        {
+            string tmp;
+            if (this.steam_ < 33)
+            {
+                tmp = "Alhainen";
+            }
+            else if (this.steam_ < 66)
+            {
+                tmp = "Sopiva";
+            }
+            else
+            {
+                tmp = "Liikaa";
+            }
+
+            Console.WriteLine("Lampotila: " + this.temp_ + "'C, Vesihoyry: " + tmp);
+        }
+    }
+}
---
+++ OOP-Harj/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Harj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kiuas k1 = new Kiuas(10, 5);
+            int valinta = new int();
+
+            do
+            {
+                Console.Write("\n[1] Laita kiuas paalle/pois paalta\n" +
+                    "[2] Lisaa lampotilaa\n" +
+                    "[3] Lisaa vesihoyrya\n" +
+                    "[4] Tulosta kiukaan tiedot\n" +
+                    "[5] Lopeta\n");
+
+                valinta = int.Parse(Console.ReadLine());
+
+                switch (valinta)
+                {
+                    case 5:
+                        break;
+                    case 1:
+                        k1.TurnOnOff();
+                        break;
+                    case 2:
+                        Console.Write("Kuinka paljon lisataan lampotilaa: ");
+                        k1.SetTemp(uint.Parse(Console.ReadLine()));
+                        break;
+                    case 3:
+                        Console.Write("Kuinka paljon lisataan vesihoyrya: ");
+                        k1.SetSteam(uint.Parse(Console.ReadLine()));
+                        break;
+                    case 4:
+                        k1.PrintInfo();
+                        break;
+                }
+
+            } while (valinta != 5);
+        }
+    }
+}
