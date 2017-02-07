baa00f1ea12f446ff1367e200d56cd97801798c5
Sami Kauhala <K8917@Labranet.jamk.fi>
2017-02-07 15:06:15 +0200

    muutamia muutoksia

---
+++ OOP-Harj/Ajoneuvo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Harj
+{
+    class Rengas
+    {
+        public int Koko { get; set; }
+        public Rengas() { Koko = 10; }
+    }
+    class Ajoneuvo
+    {
+        public List<Rengas> Renkaat;
+        public Ajoneuvo()
+        {
+            Renkaat = new List<Rengas>();
+        }
+    }
+}
